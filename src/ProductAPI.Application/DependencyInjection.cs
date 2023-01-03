be440cf9124b07880dd6a67fc36cba4c18bcbcef
Carlos Antonio Romero Gil <carlos.romero@sofka.local>
2023-01-03 11:58:59 -0500

    feat: Added Dependency Injection for mapper, mediatr and validator

---
+++ src/ProductAPI.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ProductAPI.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCoreDependencies(this IServiceCollection services)
+    {
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        return services;
+    }
+}
